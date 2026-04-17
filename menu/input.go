c09ad2afa420b0e4a558bf05a0672d5f9785d3ad
ewy <ewy0@protonmail.com>
2026-04-17 13:20:45 +0200

    allow left/right (h/l) to leap through sources instead of going one by one

--- menu/input.go
+++ menu/input.go
@@ -5,6 +5,10 @@ import tea "github.com/charmbracelet/bubbletea"
 func (m *Model) HandleInput(msg tea.KeyMsg) (tea.Cmd, error) {
 	var cmd tea.Cmd
 	switch msg.String() {
+	case "h", "left":
+		m.Leap(-1)
+	case "l", "right":
+		m.Leap(1)
 	case "up", "k":
 		m.Index--
 	case "down", "j":
@@ -19,4 +23,20 @@ func (m *Model) HandleInput(msg tea.KeyMsg) (tea.Cmd, error) {
 	m.Validate()
 
 	return cmd, nil
+}
+
+func (m *Model) Leap(direction int) {
+	for {
+		source, target := m.Result()
+		m.Index += direction
+		m.Validate()
+		newSource, newTarget := m.Result()
+		if target == newTarget {
+			return
+		}
+
+		if source != newSource {
+			return
+		}
+	}
 }
