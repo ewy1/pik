27533b56a7fe7b31a3d9da8786a96843e2b6ee71
ewy <ewy0@protonmail.com>
2026-04-22 21:07:37 +0200

    how embarrassing!

--- flags/flags.go
+++ flags/flags.go
@@ -5,7 +5,7 @@ import "github.com/spf13/pflag"
 var (
 	Here    = pflag.BoolP("here", "h", false, "run target in current directory instead of source location")
 	At      = pflag.StringP("at", "@", "", "override run location")
-	Single  = pflag.BoolP("singgle", "s", false, "do not run any triggers")
+	Single  = pflag.BoolP("single", "s", false, "do not run any triggers")
 	All     = pflag.BoolP("all", "a", false, "get sources from cache instead of crawling")
 	Dry     = pflag.BoolP("dry", "d", false, "print cmdlines instead of running them")
 	Root    = pflag.BoolP("root", "r", false, "run targets (including triggers) with sudo")
